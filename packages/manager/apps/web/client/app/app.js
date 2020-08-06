7b8902d5ad72750134d49ee4323f05c20a35f342
Marie JONES <marie.jones@corp.ovh.com>
2020-08-06 09:46:06 +0200

    feat: load translations before state is entered
    This ensures that translations can be used in the state's resolve
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/app.js
+++ packages/manager/apps/web/client/app/app.js
@@ -550,6 +550,11 @@ angular
         unregisterHook();
       });
     },
+  )
+  .run(
+    /* @ngInject */ ($translate, $transitions) => {
+      $transitions.onBefore({ to: 'app.**' }, () => $translate.refresh());
+    },
   );
 
 export default moduleName;
