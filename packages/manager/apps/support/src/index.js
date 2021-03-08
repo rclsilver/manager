88da12d0b847656c6c8b4a3248d463cfc41206a3
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-03-08 16:31:07 +0100

    feat(environment): use ufrontend to bootstrap application
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
    
    BREAKING CHANGE: use ufrontend to bootstrap application
      the environment is no more a singleton
      the environment is provided by ufrontend
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/manager/apps/support/src/index.js
@@ -0,0 +1,22 @@
+import 'script-loader!jquery'; // eslint-disable-line
+import 'whatwg-fetch';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'angular';
+import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
+
+import registerApplication from '@ovh-ux/ufrontend/application';
+import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
+
+attachPreloader(findAvailableLocale(detectUserLocale()));
+
+registerApplication('hub').then(({ environment }) => {
+  environment.setVersion(__VERSION__);
+
+  import(`./config-${environment.getRegion()}`)
+    .catch(() => {})
+    .then(() => import('./app.module'))
+    .then(({ default: startApplication }) => {
+      startApplication(document.body, environment);
+    });
+});
