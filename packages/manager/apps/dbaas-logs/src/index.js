75c47460f2ecefd47b5673b56f7e83c4ae7e45f0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-03-08 11:53:34 +0100

    feat(environment): use ufrontend to bootstrap application
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
    
    BREAKING CHANGE: use ufrontend to bootstrap application
      the environment is no more a singleton
      the environment is provided by ufrontend
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/dbaas-logs/src/index.js
+++ packages/manager/apps/dbaas-logs/src/index.js
@@ -2,17 +2,17 @@ import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
 import 'core-js/stable';
 import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
-import { bootstrapApplication } from '@ovh-ux/manager-core';
+import registerApplication from '@ovh-ux/ufrontend/application';
+import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
 
-attachPreloader();
+attachPreloader(findAvailableLocale(detectUserLocale()));
 
-bootstrapApplication().then(({ region }) => {
-  import(`./config-${region}`)
+registerApplication('dbaas-logs').then(({ environment }) => {
+  environment.setVersion(__VERSION__);
+  import(`./config-${environment.getRegion()}`)
     .catch(() => {})
     .then(() => import('./app.module'))
-    .then(({ default: application }) => {
-      angular.bootstrap(document.body, [application], {
-        strictDi: true,
-      });
+    .then(({ default: startApplication }) => {
+      startApplication(document.body, environment);
     });
 });
