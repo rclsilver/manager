05aa56f37c54b672dfe2f8a76c984c49357c34aa
frenauvh <florian.renaut@corp.ovh.com>
2021-03-15 15:52:20 +0100

    fix(billing): disable anticipate renew for xdsl services
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/billing/src/components/services-actions/services-actions.controller.js
+++ packages/manager/modules/billing/src/components/services-actions/services-actions.controller.js
@@ -68,6 +68,12 @@ export default class ServicesActionsCtrl {
     }
   }
 
+  canAnticipateRenew() {
+    return !['PACK_XDSL', 'TELEPHONY', 'XDSL'].includes(
+      this.service.serviceType,
+    );
+  }
+
   getRenewUrl() {
     return `${RENEW_URL[this.user.ovhSubsidiary] || RENEW_URL.default}${
       this.service.serviceId
