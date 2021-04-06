7bd796f0ad73988cf6386c2a2160d772eb20364d
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-04-06 15:21:01 +0200

    feat(core): add cdn guides link
    ref: MANAGER-6586
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/modules/core/src/manager-core.constants.js
+++ packages/manager/modules/core/src/manager-core.constants.js
@@ -265,6 +265,26 @@ export const URLS = {
       SN: 'https://docs.ovh.com/fr/',
       TN: 'https://docs.ovh.com/fr/',
     },
+    cdn: {
+      CA:
+        'https://docs.ovh.com/ca/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      DE:
+        'https://docs.ovh.com/de/hosting/verwendung_des_geocache_boosters_auf_einem_webhosting/',
+      ES:
+        'https://docs.ovh.com/es/hosting/guia_de_uso_del_acelerador_geocache_en_un_alojamiento_web/',
+      FR:
+        'https://docs.ovh.com/fr/hosting/accelerer-mon-site-web-en-utilisant-le-cdn/',
+      GB:
+        'https://docs.ovh.com/gb/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      IE:
+        'https://docs.ovh.com/ie/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      IT:
+        'https://docs.ovh.com/it/hosting/guida_allutilizzo_dellacceleratore_geocache_su_un_hosting_web/',
+      PL:
+        'https://docs.ovh.com/pl/hosting/przewodnik_dotyczacy_uslugi_geocache_na_hostingu_www/',
+      PT:
+        'https://docs.ovh.com/pt/hosting/guia_de_utilizacao_do_acelerador_geocache_num_alojamento_web/',
+    },
     cda: 'storage/',
     ip_failover: {
       CZ: {
